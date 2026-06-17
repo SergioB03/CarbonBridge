14e00ce71ec9dfb856201c6ece63bcb28ab2d1e2
Ahenriquez28 <ahenriquez200528@gmail.com>
2026-06-16 22:27:55 -0400

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/carbon/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
